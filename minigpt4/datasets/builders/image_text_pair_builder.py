917ccaa8de9c3e6a3d69301fd30cb49e7ef2d46d
ngocson1042002 <20280080@student.hcmus.edu.vn>
2024-06-07 01:07:39 +0700

    add some config

--- minigpt4/datasets/builders/image_text_pair_builder.py
+++ minigpt4/datasets/builders/image_text_pair_builder.py
@@ -18,7 +18,32 @@ from minigpt4.datasets.datasets.aok_vqa_datasets import AOKVQADataset
 from minigpt4.datasets.datasets.coco_vqa_datasets import COCOVQADataset
 from minigpt4.datasets.datasets.ocrvqa_dataset import OCRVQADataset
 from minigpt4.datasets.datasets.coco_caption import COCOCapDataset
+from minigpt4.datasets.datasets.vindrcxr import VinDrCXRDataset
 
+@registry.register_builder("vindrcxr_grounded_diseases")
+class GroundedCaptionBuilder(BaseDatasetBuilder):
+    train_dataset_cls = VinDrCXRDataset
+    DATASET_CONFIG_DICT = {
+        "default": "configs/datasets/flickr/default.yaml",
+    }
+
+    def build_datasets(self):
+        # at this point, all the annotations and image/videos should be all downloaded to the specified locations.
+        logging.info("Building datasets...")
+        self.build_processors()
+        build_info = self.config.build_info
+        datasets = dict()
+
+        # create datasets
+        dataset_cls = self.train_dataset_cls
+        datasets['train'] = dataset_cls(
+            vis_processor=self.vis_processors["train"],
+            text_processor=self.text_processors["train"],
+            ann_path=build_info.ann_path,
+            vis_root=build_info.image_path,
+        )
+
+        return datasets
 
 @registry.register_builder("multitask_conversation")
 class MultitaskConversationBuilder(BaseDatasetBuilder):
